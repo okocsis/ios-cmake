584686516e2fee4bc9c61f03cc74dac54e2e9733
Oliver Kocsis <oliver@numbereight.me>
2019-06-05 19:17:27 +0100

    wip3

--- samples/build.sh
+++ samples/build.sh
@@ -3,7 +3,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 pushd "$DIR" > /dev/null
 rm -rf ./Hello/build/
+rm -rf ./install
+mkdir install
 cmake -S./Hello -B./Hello/build -GXcode -DCMAKE_TOOLCHAIN_FILE=iOS.cmake
-cmake --build ./Hello/build --config Release --target install
+cmake DESTDIR="./install" --build ./Hello/build --config Release --target install
 
 popd > /dev/null
