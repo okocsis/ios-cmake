dc55ad5a62682a2fc0352086e79e98bd2ad1ea4d
Oliver Kocsis <oliver@numbereight.me>
2019-06-05 17:54:09 +0100

    wip2

--- samples/build.sh
+++ samples/build.sh
@@ -2,8 +2,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 pushd "$DIR" > /dev/null
-rm -rf ./hello-lib/build/
-cmake -S./hello-lib -B./hello-lib/build -GXcode -DCMAKE_TOOLCHAIN_FILE=iOS.cmake -DCMAKE_INSTALL_PREFIX=`pwd`/_install
-cmake --build ./hello-lib/build --config Release --target install
+rm -rf ./Hello/build/
+cmake -S./Hello -B./Hello/build -GXcode -DCMAKE_TOOLCHAIN_FILE=iOS.cmake
+cmake --build ./Hello/build --config Release --target install
 
 popd > /dev/null
