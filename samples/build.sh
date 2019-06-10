ca61d83725bc5b1a755928f4592badbd9a8b37f4
Oliver Kocsis <oliver@numbereight.me>
2019-06-10 15:11:50 +0100

    bka

--- samples/build.sh
+++ samples/build.sh
@@ -2,10 +2,23 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 pushd "$DIR" > /dev/null
-rm -rf $DIR/Hello/build/
-rm -rf $DIR/install
-mkdir $DIR/install
-cmake -S$DIR/Hello -B$DIR/Hello/build -DCMAKE_INSTALL_PREFIX="$DIR/install" -DCMAKE_TOOLCHAIN_FILE=iOS.cmake -GXcode
-cmake --build $DIR/Hello/build --config Release --target install
+PROJ_DIR="$DIR/Hello/"
+BUILD_DIR="$DIR/build/"
+INSTALL_DIR="$DIR/install"
+
+rm -rf $BUILD_DIR
+rm -rf $INSTALL_DIR
+mkdir $INSTALL_DIR
+
+cmake -S$PROJ_DIR \
+      -B$BUILD_DIR \
+      -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
+      -DBUILD_SHARED_LIBS="OFF" \
+      -DCMAKE_TOOLCHAIN_FILE=iOS.cmake \
+      -GXcode
+
+cmake --build $BUILD_DIR \
+      --config Release \
+      --target install
 
 popd > /dev/null
